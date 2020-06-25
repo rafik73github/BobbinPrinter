b872e1c12b4490472dcaa09d63afd5c605e8b80e
RAF <rafal.michal.rogowski@gmail.com>
2020-06-25 13:13:36 +0200

    Dodaj pliki projektów.

---
+++ BobbinPrinter/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BobbinPrinter
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddPrintButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextillListButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MainMenuExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+    }
+}
